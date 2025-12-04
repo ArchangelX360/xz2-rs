7075d16f01d42c3e2af4bc4da36e89487f6b3da3
Titouan Bion <titouan.bion@jetbrains.com>
2025-12-04 17:16:05 +0100

    Attempt to fix path for inclusion of `config.h` 3

--- lzma-sys/build.rs
+++ lzma-sys/build.rs
@@ -52,7 +52,7 @@ fn main() {
     build
         .files(src_files)
         // all C preproc defines are in `./config.h`
-        .define("HAVE_CONFIG_H", "1")
+        .define("HAVE_CONFIG_H", "0")
         .include("xz-5.2/src/liblzma/api")
         .include("xz-5.2/src/liblzma/lzma")
         .include("xz-5.2/src/liblzma/lz")
@@ -62,6 +62,7 @@ fn main() {
         .include("xz-5.2/src/liblzma/common")
         .include("xz-5.2/src/liblzma/rangecoder")
         .include("xz-5.2/src/common")
+        .include(env::current_dir().unwrap())
         .include(".");
 
     let paths = fs::read_dir(".").unwrap();
